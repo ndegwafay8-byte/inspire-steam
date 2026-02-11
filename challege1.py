c7c4f0ff0c09adaa5451236d8ef5e3a805e4da15
ndegwafay8-byte <ndegwafay8@gmail.com>
2026-02-11 09:42:21 +0300

    first commit

---
+++ challege1.py
@@ -0,0 +1,8 @@
+university = "utalii"
+grade = "C"
+course ="catering"
+cluster_points = "56"
+print(f"my fovourite university is {university}")
+print(f"my score in kcse is {grade}")
+print(f"my favourite course is {course}")
+print(f"my cluster points are {cluster_points}")
---
+++ hello.py
@@ -0,0 +1,14 @@
+# this is a comment
+print("hello from Faith")
+# city/town
+print("i am from utawala")
+# my hobbies
+print("i love to cooking play instrument")
+age=18
+height=5.2
+weight=60
+
+print(f"i am {age} years old")
+print(f"i am also {height} metres tall")
+print(f"i am also {weight} kgs")
+
